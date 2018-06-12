98b60ab9b35c7fe393b98b5f88735e75107f74c7
Isuru <isuru71@gmail.com>
2018-06-12 22:43:06 +0530

    initial server configurations

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    db: {
+        uri: 'mongodb://test:undefined123@ds257640.mlab.com:57640/pharmacy'
+    }
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express')
+var mongoose = require('mongoose')
+var bodyparser = require('body-parser')
+
+var config = require('./config')
+
+var app = express()
+
+app.use(bodyparser.json())
+
+mongoose.connect(config.db.uri, (err) => {
+    if (err) {
+        console.log('database err - ', err)
+        process.exit(-1)
+    }
+    console.log('database connected')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hell World')
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
