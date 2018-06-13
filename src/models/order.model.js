e7cf5777e4c05920816a83e46069b046f2f75d55
GeethSameera <geethsameera077@gmail.com>
2018-06-13 19:23:03 +0530

    inventory routes implementation

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,39 +1,45 @@
 var mongoose=require('mongoose');
 
 var orderSchema=new mongoose.Schema({
+
     orderID:{
         type :String,
         required:[true,'OrderId isn required']
     },
+
     orderDate:{
         type:String,
         required:[true,'orderDate is required']
     },
+
     dueDate:{
         type:String,
         required:[true,'due date is required']
     },
-    items:{
-            medicineID:{
-                type:String,
-                required:[true,'medicineID is required']
-                },
-            qty:{
-                type:String,
-            },
-            company:{
-                type:String,
-                required:[true,'company is required']
+
+    items:[{
+        medicineID: {
+            type: String,
+            required: [true, 'medicineID is required']
         },
-            unitPrice:String,
-            subTotal:String,
+        qty: {
+            type: String,
         },
+        company: {
+            type: String,
+            required: [true, 'company is required']
+        },
+        unitPrice: String,
+        subTotal: String,
+    }],
     supplier:{
         type:String,
         required:[true,'supplier required']
     },
     total:String,
     discount:String,
+    netTotal:String,
+    orderStatus:String
 
 });
 
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -2,25 +2,51 @@ var express=require('express');
 var router=express.Router();
 var bodyparser=require('body-parser');
 
+var Order=require('../models/order.model');
 
-router.post('/new',(req,res,next)=>{
-    res.json({message:'hi'});
+
+router.post('/new',function (req,res,next) {
+    var order=new Order(req.body);
+    order.save().then(function () {
+        res.send(order);
+    }).catch(next)
 });
 
 router.post('/pay/:id',(req,res,next)=>{
-    res.json({message:'hi'});
+    Order.find({orderID:req.params.id}).then((data)=>{
+        data.orderStatus='Received/payed';
+        order.findByIdAndUpdate({supplierID:req.params.id},data).then(function (data) {
+            res.json(data);
+        });
+
+    });
+});
+
+router.get('/view',(req,res,next)=>{
+    Order.find().then((data)=>{
+       res.json(data);
+    });
 });
 
 router.get('/view/:id',(req,res,next)=>{
-    res.send('hi');
+    Order.find({orderID:req.params.id}).then((data)=>{
+        res.json(data);
+    });
 });
 
 router.put('/update/:id',(req,res,next)=>{
+    Order.findByIdAndUpdate({orderID:req.params.id},req.body).then(function () {
+        Order.findOne({orderID:req.params.id}).then(function (order) {
+            res.send(order);
+        });
 
+    });
 });
 
 router.delete('/delete/:id',(req,res,next)=>{
-
+    Order.findByIdAndRemove({orderID:req.params.id}).then(function (order) {
+        res.send(order);
+    });
 });
 
 module.exports=router;
