8bc01f3e32d0d49b00fc0f7297f4dcc2272e32bd
Isuru <isuru71@gmail.com>
2018-06-14 10:21:17 +0530

    patient model

--- src/models/patients.model.js
+++ src/models/patients.model.js
@@ -1,7 +1,33 @@
 var mongoose = require('mongoose');
 
 var Patient = new mongoose.Schema({
-
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    paitentId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    NIC: {
+        type: String,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    contactNumber: {
+        type: Number,
+        required: true
+    }
 })
 
 exports.modules = mongoose.model('Patient', Patient);
